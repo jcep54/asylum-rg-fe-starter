55bff667e92fc7c8fe0c9fe3e037c3c1e6e8720b
Jose Cepero <ceperojose54@gmail.com>
2023-12-02 14:29:36 -0500

    wip added content for bottom section

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -68,7 +68,6 @@ function RenderLandingPage(props) {
           <Button
             type="default"
             style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
-            onClick={() => history.push('/graphs')}
           >
             Download the Data
           </Button>
@@ -94,6 +93,39 @@ function RenderLandingPage(props) {
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
         {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <div className="section-title">
+            <h2>Systematic Disparity Insights</h2>
+          </div>
+
+          <div className="insight-continer">
+            <div className="insight info">
+              <h2>36%</h2>
+              <p>
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </p>
+            </div>
+
+            <div className="insight info">
+              <h2>5%</h2>
+              <p>
+                The New York asylum office grant rate dropped to 5 percent in
+                fiscal year 2020.
+              </p>
+            </div>
+
+            <div className="insight info">
+              <h2>6x Lower</h2>
+              <p>
+                Between fiscal year 2017 and 2020, the New York asylum office’s
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </p>
+            </div>
+          </div>
+        </div>
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
